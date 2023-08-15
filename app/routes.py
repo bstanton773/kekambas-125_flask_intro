a215fcfa2f8ed627fdd772c9943d7faae0a3a87a
Brian <bstanton773@gmail.com>
2023-08-15 16:14:58 -0500

    Add user to database on signup

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 from flask import render_template, redirect, url_for
 from app.forms import SignUpForm
+from app.models import User
 
 # Add a route
 @app.route('/')
@@ -17,8 +18,13 @@ def signup():
         username = form.username.data
         email = form.email.data
         password = form.password.data
-        print(first_name, last_name, username, email, password)
+        # print(first_name, last_name, username, email, password)
 
+        # Create a new instance of the User class with data from form
+        new_user = User(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+        # Add the new_user object to the database
+        db.session.add(new_user)
+        db.session.commit()
         # redirect back to the home page
         return redirect(url_for('index'))
     
