98fb6729002e7d90075ebf1922b61230da6a5a69
Brian <bstanton773@gmail.com>
2023-08-17 13:41:28 -0500

    Get Post by ID endpoint

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -7,3 +7,12 @@ from app.models import Post
 def get_posts():
     posts = db.session.execute(db.select(Post)).scalars().all()
     return [post.to_dict() for post in posts]
+
+
+@api.route('/posts/<post_id>')
+def get_post(post_id):
+    post = db.session.get(Post, post_id)
+    if post:
+        return post.to_dict()
+    else:
+        return {'error': f'Post with an ID of {post_id} does not exist'}, 404
