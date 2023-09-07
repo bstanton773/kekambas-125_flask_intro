822cf3fedac94863d94ec28ee0820491511dea5a
Brian <bstanton773@gmail.com>
2023-09-07 13:49:47 -0500

    Add Create User Endpoint

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,6 +1,6 @@
 from . import api
 from app import db
-from app.models import Post
+from app.models import Post, User
 from flask import request
 from .auth import basic_auth, token_auth
 
@@ -104,3 +104,39 @@ def delete_post(post_id):
 def get_me():
     me = token_auth.current_user()
     return me.to_dict()
+
+@api.route('/users', methods=["POST"])
+def create_user():
+    # Check to see that the request body is JSON
+    if not request.is_json:
+        return {'error': 'Your content-type must be application/json'}, 400
+    # Get the data from the request body
+    data = request.json
+    # Validate incoming data
+    required_fields = ['firstName', 'lastName', 'username', 'email', 'password']
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+    if missing_fields:
+        return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
+    
+    # Get the data from the body
+    first_name = data.get('firstName')
+    last_name = data.get('lastName')
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    # Check is a user with that username or email already exists
+    check_user = db.session.execute(db.select(User).where((User.username == username) | (User.email == email))).scalar()
+    if check_user:
+        return {'error': f"A user with that username and/or email already exists"}, 400
+
+    # Create a new User instance with the data
+    new_user = User(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+    # add to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return new_user.to_dict(), 201   
